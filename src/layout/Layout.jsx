8865851ac1cf2f66bae96d5377ba239daabbbcdc
alisher152 <slasher.abdullaev601@gmail.com>
2025-03-14 19:49:53 +0500

    Добавлена страницы Home и Footer

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,14 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import NavbarHome from "../components/NavbarHome";
 import Footer from "../components/Footer";
 
 function Layout() {
-  const location = useLocation();
-  const isHomePage = location.pathname === "/";
+  //const location = useLocation();
+  //const isHomePage = location.pathname === "/";
 
   return (
     <>
-      {isHomePage ? <NavbarHome /> : <Navbar />}
+      <Navbar />
       <Outlet /> {/* Вставляем динамический контент */}
       <Footer />
     </>
